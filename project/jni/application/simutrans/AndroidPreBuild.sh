76fc1703dddbdc10ea69a692073d4cc454c3ca09
Alexis He <ahe.krosk@gmail.com>
2021-08-30 08:58:59 +0200

    Add: two default paks for install window to appear

--- project/jni/application/simutrans/AndroidPreBuild.sh
+++ project/jni/application/simutrans/AndroidPreBuild.sh
@@ -11,8 +11,11 @@ fi
 
 echo Generating data.zip
 mkdir -p AndroidData
-cd simutrans/simutrans
-[ -e music/TimGM6mb.sf2 ] || wget https://sourceforge.net/p/mscore/code/HEAD/tree/trunk/mscore/share/sound/TimGM6mb.sf2?format=raw -O music/TimGM6mb.sf2
+cd simutrans
+[ -e simutrans/pak ] || (wget --show-progress -nc https://downloads.sourceforge.net/project/simutrans/pak64/122-0/simupak64-122-0.zip && unzip -n simupak64-122-0.zip)
+[ -e simutrans/pak64.german ] || (wget --show-progress -nc http://simutrans-germany.com/pak.german/pak64.german_0-122-0-0-2_full.zip && unzip -n pak64.german_0-122-0-0-2_full.zip)
+cd simutrans
+[ -e music/TimGM6mb.sf2 ] || wget -nc --show-progress https://sourceforge.net/p/mscore/code/HEAD/tree/trunk/mscore/share/sound/TimGM6mb.sf2?format=raw -O music/TimGM6mb.sf2
 rm -f ../../AndroidData/data.zip
 zip -r -0 ../../AndroidData/data.zip * >/dev/null
 cd ../..
