472a6db94fdf27dc467d0275be76f42a31366064
krosk <ahe.krosk@gmail.com>
2021-09-17 13:11:32 +0000

    Add: additional files

--- project/jni/Android.mk
+++ project/jni/Android.mk
@@ -12,19 +12,4 @@ endif
 
 NDK_PATH := $(shell dirname $(shell which ndk-build))
 
-NDK_SUBDIR_MAKEFILES_FULL := $(call all-subdir-makefiles)
-
-# If you want to exclude certain subprojects from the build process.
-# v.g.: SDL2_image already brings it's own implementation of png, so we exclude the bundled one
-ifeq ($(SDL_VERSION),2.0)
-BLACKLISTED_SUBPROJECTS := jpeg png ogg mpg123 timidity fluidsynth faad openal sdl_blitpool sdl_gfx sdl_image sdl_main sdl_mixer sdl_net sdl_sound sdl_ttf zzip freetype
-else
-BLACKLISTED_SUBPROJECTS := sdl2_image
-endif
-
-BLACKLISTED_MAKEFILES := $(addprefix jni/../jni/,$(BLACKLISTED_SUBPROJECTS))
-BLACKLISTED_MAKEFILES := $(addsuffix /Android.mk,$(BLACKLISTED_MAKEFILES))
-
-NDK_SUBDIR_MAKEFILES := $(filter-out $(BLACKLISTED_MAKEFILES), $(NDK_SUBDIR_MAKEFILES_FULL))
-
-include $(NDK_SUBDIR_MAKEFILES)
+include $(call all-subdir-makefiles)
