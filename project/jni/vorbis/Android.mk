c535d2eb41d590fe6ad43496cb0301f743c4992d
krosk <ahe.krosk@gmail.com>
2021-09-18 09:16:46 +0000

    Change: merge vorbis and vorbisenc
    (cherry picked from commit f13e230d33e1ea49049f4f38aa1222e95e4fb0d0)

--- project/jni/vorbis/Android.mk
+++ project/jni/vorbis/Android.mk
@@ -1,13 +1,27 @@
 LOCAL_PATH := $(call my-dir)
 
+########################
+
 include $(CLEAR_VARS)
 
-ECHO := $(shell wget -nc https://github.com/FluidSynth/fluidsynth/releases/download/v2.2.2/fluidsynth-2.2.2-android.zip; unzip -n fluidsynth-2.2.2-android.zip *libvorbis.so -d $(LOCAL_PATH); )
+ECHO := $(shell wget -nc https://github.com/FluidSynth/fluidsynth/releases/download/v2.2.2/fluidsynth-2.2.2-android.zip; unzip -n fluidsynth-2.2.2-android.zip *libvorbis*.so -d $(LOCAL_PATH); )
 
 LOCAL_MODULE := vorbis
 
 LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libvorbis.so
 
 LOCAL_SHARED_LIBRARIES := ogg
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+########################
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := vorbisenc
+
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libvorbisenc.so
+
+LOCAL_SHARED_LIBRARIES := ogg vorbis
 
 include $(PREBUILT_SHARED_LIBRARY)
