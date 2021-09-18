729f0be36a36c69b5d17b94adf2d349a4527c7ad
krosk <ahe.krosk@gmail.com>
2021-09-18 19:05:02 +0000

    Revert previous commit

--- project/jni/application/simutrans/AndroidBuild.sh
+++ project/jni/application/simutrans/AndroidBuild.sh
@@ -7,7 +7,7 @@ ln -sf libbzip2.so ../../../../obj/local/$1/libbz2.so
 rm -f config.$1.txt
 echo VERBOSE=1 >> config.$1.txt
 echo OPTIMIZE=1 >> config.$1.txt
-echo OSTYPE=android >> config.$1.txt
+echo OSTYPE=linux >> config.$1.txt
 echo COLOUR_DEPTH=16 >> config.$1.txt
 echo BACKEND=sdl2 >> config.$1.txt
 echo USE_SOFTPOINTER=1 >> config.$1.txt
