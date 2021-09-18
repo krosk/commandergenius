d4ddaf390d789502c76ff5a1f76dae1844a608dd
Alexis He <ahe@slb.com>
2021-09-18 08:56:48 +0000

    Change: merge glib so into a single folder
    (cherry picked from commit 01ae25b39b3cdf0d3e97ffc07b3e4b7fb95b7a3b)

--- project/jni/glib-2.0/Android.mk
+++ project/jni/glib-2.0/Android.mk
@@ -1,13 +1,39 @@
 LOCAL_PATH := $(call my-dir)
 
+#####################
+
 include $(CLEAR_VARS)
 
-ECHO := $(shell wget -nc https://github.com/FluidSynth/fluidsynth/releases/download/v2.2.2/fluidsynth-2.2.2-android.zip; unzip -n fluidsynth-2.2.2-android.zip *libglib-2.0.so -d $(LOCAL_PATH); )
+ECHO := $(shell wget -nc https://github.com/FluidSynth/fluidsynth/releases/download/v2.2.2/fluidsynth-2.2.2-android.zip; unzip -n fluidsynth-2.2.2-android.zip *libg*-2.0.so -d $(LOCAL_PATH); )
 
 LOCAL_MODULE := glib-2.0
 
 LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libglib-2.0.so
 
 LOCAL_SHARED_LIBRARIES := 
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+#####################
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := gobject-2.0
+
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libgobject-2.0.so
+
+LOCAL_SHARED_LIBRARIES := glib-2.0
+
+include $(PREBUILT_SHARED_LIBRARY)
+
+#####################
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := gthread-2.0
+
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libgthread-2.0.so
+
+LOCAL_SHARED_LIBRARIES := glib-2.0
 
 include $(PREBUILT_SHARED_LIBRARY)
